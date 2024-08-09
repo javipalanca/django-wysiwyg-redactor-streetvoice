9b0ee6362e098eccca938626f01b83dda461185c
Javi Palanca <jpalanca@gmail.com>
2024-08-09 08:22:48 +0200

    Use re_path in urls.py

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,16 +1,16 @@
 
-from django.urls import path
+from django.urls import re_path
 
 from redactor.views import RedactorUploadView
 from redactor.forms import FileForm, ImageForm
 
 
 urlpatterns = [
-    path(r'^upload/image/(?P<upload_to>.*)',
+    re_path(r'^upload/image/(?P<upload_to>.*)$', 
         RedactorUploadView.as_view(form_class=ImageForm),
         name='redactor_upload_image'),
 
-    path(r'^upload/file/(?P<upload_to>.*)',
+    re_path(r'^upload/file/(?P<upload_to>.*)$', 
         RedactorUploadView.as_view(form_class=FileForm),
         name='redactor_upload_file'),
 ]
